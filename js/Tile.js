518be8a1177444706d64756fea6ca74f302fd1a9
Guillaume Le Biller <guillaume.lebiller@gmail.com>
2013-06-02 00:14:32 +0200

    Cleaning.

--- js/Tile.js
+++ js/Tile.js
@@ -2,12 +2,10 @@ var Tile = function(width, height, modifier) {
     this.tile = $(
         '<div class="flip" data-flipped="false">\
                         <div class="face front">\
-                            <img src="/img/hexa.png" width="'
-            + width + '" height="' + height + '">\
+                            <img src="/img/hexa.png" width="' + width + '" height="' + height + '">\
                         </div>\
                         <div class="face back">\
-                            <img src="/img/hexa_flipped.png" width="'
-            + width + '" height="' + height + '" />\
+                            <img src="/img/hexa_flipped.png" width="' + width + '" height="' + height + '" />\
                         </div>\
                     </div>'
     );
